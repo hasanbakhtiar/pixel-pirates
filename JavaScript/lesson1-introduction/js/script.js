18e2d35a233117e9b5b0f0c03291f6706b0cbdc1
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-11-20 16:21:35 +0400

    lastest

---
+++ JavaScript/lesson1-introduction/js/script.js
@@ -0,0 +1,17 @@
+//console.log("Hello World");
+/*console.log("Test1");
+console.log("Test2");
+console.log("Test3");
+console.log("Test4");
+console.log("Test5");
+*/
+
+
+// debugger
+// console.log("Test1");
+// console.log("Test2");
+// console.laog("Test3");
+// console.log("Test4");
+// console.log("Test5");
+
+
---
+++ JavaScript/lesson2-datatypes/js/script.js
@@ -0,0 +1,16 @@
+// Primitive Data Types
+var info = "Hello"; //string
+var infoOne = 5; //number
+var infoTwo = true //boolean (false)
+
+// Composite Data types
+var infoThree = ["Hello",5,true]; //Array
+var infoFour = {
+    brand:"BMW",
+    year:2010,
+    newCar:false,
+}; //Object
+
+// Special Data Types
+var infoFive = undefined;
+var infoSix = null;
