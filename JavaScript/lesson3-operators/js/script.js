4af8254c5b1f2a621ea0b941a322a84a691f3168
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-11-22 17:18:44 +0400

    lastest

---
+++ JavaScript/lesson3-operators/js/script.js
@@ -0,0 +1,140 @@
+
+// assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+
+
+
+// arithmetic operators 
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+// comparison operators
+// ==
+// ===
+// !=
+// !==
+// < 
+// <= 
+// > 
+// >= 
+
+
+
+// logical operators
+// && - AND
+// || - OR
+// !  - NOT
+
+
+// string operators
+// +
+// +=
+
+
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+
+// var a = 5,  b= 10;
+// console.log(a,b);
+
+
+// \            (tərs slash)(Qaçındırma operatorudur)
+// var info = "Hasan\"s book";
+// console.log(info);
+
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+// console.log(typeof "5");
+
+
+// var a = true;
+// var b = typeof a;
+// console.log(b);
+
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+
+// var a = 5;
+// console.log(a);
+
+// console.log(void(a));
+
+
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
+
+// ECMAScript
+// var info= 5;
+// var info= 15;
+// console.log(info);
+
+
+
+// 1.Dəyişkən adları mənalı və anlaşıqlı olmalıdır.
+// 2.Dəyişkən adlarını yazarkən İngilis əlifbasından başqa əlifadan istifadə etmək qadağandır.
+// 3.Dəyişkən adları _ (alt xətt) ilə başlaya bilər.
+// 4.Dəyişkən adları $(dollar) işarəsi ilə başlaya bilər.
+// 5.Dəyişkən adları heç bir zaman rəqəm ilə başlaya bilməz.Amma ilk hərfdə sonra istifadə edə bilərik.
+// 6.Dəyişkən adları böyük kiçik hərfləri anlayır.
+// 7.Eyni dəyişkənə iki fərqli məlumat tanımlanan zaman sonuncu tanımlanan məlumatı göstərir.
+// 8.JavaScript tərəfindən hazırlanmış rezerv kəlimələri dəyişkən adlarına vermək qadağandır.
+
+// abstract 	    arguments	    await*	            boolean
+// break	        byte	        case	            catch
+// char	            class*	        const	            continue
+// debugger	        default	        delete	            do
+// double	        else	        enum*	            eval
+// export*	        extends*	    false	            final
+// finally	        float	        for	                function
+// goto	            if	            implements	        import*
+// in	            instanceof	    int	                interface
+// let*	            long	        native	            new
+// null	            package	        private	            protected
+// public	        return	        short	            static
+// super*	        switch	        synchronized	    this
+// throw	        throws	        transient	        true
+// try	            typeof	        var	                void
+// volatile	        while	        with	            yield
+
+// var      -> variable (global)
+// const    -> constant (local)
+// let      -> let      (local)
+
+
+
+
+// // global
+
+// if (true) {
+//     // 1local global
+//     if (true) {
+//         const a = 5;
+//         // 2local global
+        
+//         if (true) {
+//             // local
+//         }
+//     }
+    
+//     console.log(a);
+// }
+
+
+
