fa2f9fcced172eedbc78c257a151f63c466249b3
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-11-24 21:05:41 +0400

    lastest

---
+++ JavaScript/lesson4-condition/js/script.js
@@ -0,0 +1,105 @@
+
+// const time = 9;
+
+
+// if (time >= 5 && time < 13) {
+//     console.log("Good Morning");
+
+//     if (time === 7) {
+//         console.log("Get up");
+
+//     } else if (time === 8) {
+//         console.log("Have a breakfast");
+
+//     } else if (time === 9) {
+//         console.log("Go to school");
+
+//     }
+
+// } else if (time >= 13 && time < 17) {
+//     console.log("Good Afternoon");
+
+// } else if (time >= 17 && time <= 21) {
+//     console.log("Good Evening");
+
+// } else if (time >= 22 && time < 23) {
+//     console.log("good Night");
+
+// } else {
+//     console.log("Wrong Time");
+
+// }
+
+
+// const info = 51;
+
+// switch (info) {
+//     case 5:
+//         console.log("This is 5");
+//         break;
+//     case "Hello World":
+//         console.log("This is Hello World");
+//         break;
+//     default:
+//         console.log("This is default");
+//         break;
+// }
+
+// if (5 < 1)
+//     console.log("This is true");
+// else
+//     console.log('Tnis is false');
+
+
+let info = 13;
+
+// if (5 < info) {
+//     info = 3;
+//     switch (info) {
+//         case 3:
+//             console.log('This is true');
+
+//             break;
+
+//         default:
+//             break;
+//     }
+// }
+
+
+// switch (info) {
+//     case 13:
+//         info = 3
+//         if (info<13) {
+//             console.log('This is new true');
+            
+//         }
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+
+
+
+// try {
+//     // throw("Her seh eladi")
+//     const a = 5;
+//     b.b;
+    
+    
+// } catch (error) {
+//     console.log(error);
+    
+//     // console.log('Burda nese sehv var');
+    
+    
+// }finally{
+//     console.log('Men sham agaciyam');
+    
+// }
+
---
+++ JavaScript/lesson5-array-object/js/script.js
@@ -0,0 +1,24 @@
+// const product = ['bread','table','pen'];
+// const product = [];
+// product[0] = "table"
+// product[1] = "pen"
+// product[2] = "bread"
+// console.log(product);
+
+// const product = Array("table",'book','pen');
+// console.log(product);
+
+
+// const product = ['book','pen','table'];
+// product['info'] = 'new value'
+// console.log(product[3]);
+
+
+// const product = [1,2,['table',[true,'hello',['summer','winter']],'book','book1'],3,4,5];
+// console.log(product[2][1][2][1]);
+
+const info = prompt();
+
+const a = Number(info);
+
+console.log(typeof a);
