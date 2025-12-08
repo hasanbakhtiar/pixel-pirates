f7ad9d039fb1c95ebb0edd4d572d4a320d5467cd
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-12-08 20:37:58 +0400

    lastest

---
+++ JavaScript/lesson8-defer/js/script.js
@@ -0,0 +1,19 @@
+//  "use strict";
+
+ 
+// if (5>10) {
+//     console.log("Hello");
+    
+// }else if(20<10){
+//     console.log("Hello new");
+    
+// } else {
+//     console.log('Bye');
+    
+// }
+// ternary 
+// 5>10 ? console.log("Hello") : 20<10 ? console.log("Hello new") : console.log('Bye');
+// alert("Hey you");
+// prompt()
+// const a = confirm("Sen yaxshisan?")
+// console.log(a);
---
+++ JavaScript/lesson9-DOM-part1/js/script.js
@@ -0,0 +1,8 @@
+// console.log(document);
+
+
+const headText = document.getElementsByTagName("h1")[0];
+
+const chnageData = () => {
+    headText.innerHTML = "London"
+}
