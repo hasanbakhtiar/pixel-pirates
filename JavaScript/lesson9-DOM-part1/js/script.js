d43bf6323f12f820ac92d533215a768b04d302f8
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-12-09 14:32:57 +0400

    lastest

--- JavaScript/lesson9-DOM-part1/js/script.js
+++ JavaScript/lesson9-DOM-part1/js/script.js
@@ -3,6 +3,18 @@
 
 const headText = document.getElementsByTagName("h1")[0];
 
-const chnageData = () => {
-    headText.innerHTML = "London"
+
+const btn = document.getElementsByTagName('button')[0];
+
+
+const changeData = () => {
+  if (headText.innerHTML === "England") {
+      headText.innerHTML = "London";
+      btn.innerHTML = "show country"
+  }else{
+      headText.innerHTML = "England";
+      btn.innerHTML = "show capital"
+  }
 }
+
+btn.onclick= changeData;
