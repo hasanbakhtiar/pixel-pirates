cbb7911cf048e471cdde6699cec474539fbe928f
Hasan Bakhtiar <hasan.doctype@gmail.com>
2025-12-30 23:41:25 +0400

    lastest

---
+++ JavaScript/lesson15-BOM-part1/js/script.js
@@ -0,0 +1,45 @@
+// console.log(window.navigator.language);
+// document.querySelector('h1').innerHTML = window.navigator.language === "en-GB" ? "Hello" : "Salam";
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+
+// document.querySelector('button').onclick = ()=>{
+//     if (window.innerWidth <= 900) {
+//         console.log("Salam");
+//     } else {
+//         console.log("Bye");
+
+//     }
+// }
+
+// console.log(window.location);
+// console.log(screen.width)
+// console.log(screen.height)
+// console.log(screen.availHeight);
+// console.log(screen.availWidth);
+
+
+// setTimeout(()=>{
+//     console.log("Start");
+
+// },3000);
+
+// const action = window.setInterval(() => {
+//     document.querySelector('h1').innerHTML = new Date();
+// }, 1000);
+
+// document.querySelector('button').onclick = () => {
+//     clearInterval(action);
+// }
+
+// document.querySelector('p').style.display = "none";
+
+// setInterval(() => {
+//     document.querySelector('p').style.display = "block";
+//     document.querySelector('h1').style.display = "none";
+    
+// }, 1000);
+
+
+const ol = document.querySelector('ol');
+ol.lastChild.previousSibling.firstChild.nodeValue = "Phone"
