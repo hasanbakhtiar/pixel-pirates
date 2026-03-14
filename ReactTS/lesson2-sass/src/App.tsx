2240504193734c50a9de3d2c1c216c706bf01078
Hasan Bakhtiar <hasan.doctype@gmail.com>
2026-03-14 20:54:56 +0400

    lastest

---
+++ ReactTS/lesson2-sass/src/App.tsx
@@ -0,0 +1,36 @@
+
+const App = () => {
+  return (
+    <div>
+        <ul>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+            <li>Test</li>
+        </ul>
+    </div>
+  )
+}
+
+export default App
